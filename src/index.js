4152cd0fa9515f113fae49e38acc4a58551c8dae
Karolina Karczewska <karolcia_yahoo94@wp.pl>
2022-10-12 18:07:35 +0200

    Empty template React JS

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../Components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// import { createRoot } from 'react-dom/client';
+// const root = document.getElementById('root');
+// createRoot.render(<App tab="home" />);
